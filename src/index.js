7889937cc3d4e4041882c8657c805ebeb7ac45f5
camille renaudin <crenaudi>
2021-10-18 17:53:57 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import './index.scss'
+import { listenerCount } from 'process';
+
+const { Socket } = require('socket.io')
+const app = express()
+const http = require('http').createServer(app);
+const path = require('path')
+const port = 8080
+
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => (
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+))
+
+http.listen(port, () => {
+  console.log('listening!')
+})
 
 ReactDOM.render(
   <React.StrictMode>
